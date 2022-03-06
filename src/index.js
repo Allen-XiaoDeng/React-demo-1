0e819a5b563e86215db8c9e67165613a48b4f29f
Allen <1942813644@qq.com>
2022-03-06 08:18:56 +0800

    添加state 使用内部数据

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
-const a = 1
+const a = 0
 
 function App() {
   return (
     <div className="App">
       爸爸
-      <Son messageForson="你好"/>
+      <Son messageForson={a}/>
     </div>
   )
 }
@@ -23,28 +23,30 @@ class Son extends React.Component {
 
   add() {
     //this.staten +=1 为什么不行
-    this.setState({n: this.state.n + 1})
+    this.setState({n:this.state.n+1}) //n:1
+    this.setState((state) => { //setState 写成函数的形式
+      const n = state.n+1
+      return {n}
+    })
   }
 
   render() {
     return (
       <div className="Son">
-        我是儿子，我爸对我说{this.props.messageForson}
         儿子 n: {this.state.n}
         <button onClick={() => this.add()}>+1</button>
-        <Grandson messageForGrandson="儿子你好"/>
+        <Grandson/>
       </div>
     )
   }
 }
 
-const Grandson = (props) => {
-  const [n, setN] = React.useState(0)
+const Grandson = () => {
+  const [n, setN] = React.useState(0) //n用来读，setN用来写
   return (
     <div className="Grandson">
       孙子 n:{n}
       <button onClick={() => setN(n + 1)}>+1</button>
-      我是孙子，我爸对我说 {props.messageForGrandson}
     </div>
   )
 }
